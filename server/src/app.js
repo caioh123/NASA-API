0200a7278731c3dd20b2cc32ce4cf1373cccbc8a
caioh123 <caionwebdev@gmail.com.br>
2022-08-03 09:17:43 -0300

    adicionado cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,18 @@
-const express= require("express")
-const planetsRouter = require("./routes/planets/planets.router")
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const planetsRouter = require("./routes/planets/planets.router");
 
-app.use(express.json())
-app.use(planetsRouter)
+const app = express();
 
-module.exports = app
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+app.use(express.json());
+app.use(planetsRouter);
+
+
+
+
+
+module.exports = app;
