680adcff0c9a2d77734367450509d46c9fae65d1
caioh123 <caionwebdev@gmail.com.br>
2022-08-06 21:37:41 -0300

    planet model added

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 const PORT = process.env.PORT || 8000;
 const http = require("http");
 
-
-const app = require('./app')
+const app = require("./app");
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log("ligado");
-});
+const { loadPlanetsData } = require("./models/planets.model");
+
+async function startServer() {
+  await loadPlanetsData()
+  
+  server.listen(PORT, () => {
+    console.log("ligado");
+  });
+
+}
+
+startServer()
