8bd3e6c4b93cc834a9be49ce1f8f9d917bdab14e
caioh123 <caionwebdev@gmail.com>
2022-08-07 18:57:25 -0300

    adicionado morgan e build

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(morgan("combined"));
+
 app.use(express.json());
+
 app.use(planetsRouter);
 
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
-
-
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 module.exports = app;
